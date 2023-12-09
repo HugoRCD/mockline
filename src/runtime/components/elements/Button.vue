2525c7da28182f44cebeeb9f331e598729776f5f
HugoRCD <hugo.richard@epitech.eu>
2023-12-09 17:51:43 +0100

    Move from root to docs all the app

---
+++ src/runtime/components/elements/Button.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="px-4 py-2 text-white bg-purple-500 rounded-md">
+    Hello World from Mockline
+  </button>
+</template>
+
+<style scoped lang="scss">
+</style>
